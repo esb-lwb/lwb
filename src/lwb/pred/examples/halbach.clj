a0056557bbe6346f581bb33e060503d2174cf43b
esb-dev <Burkhardt.Renz@mni.thm.de>
2016-02-09 13:42:27 +0100

    examples from the bokk of Volker Halbach

---
+++ src/lwb/pred/examples/halbach.clj
@@ -0,0 +1,76 @@
+; lwb Logic WorkBench -- Predicate logic
+
+; Copyright (c) 2016 Burkhardt Renz, THM. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php).
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+
+(ns lwb.pred.examples.halbach
+  (:require [lwb.pred :refer (make-pred sig-from-model wff? eval-phi)]))
+
+;; Examles from Volker Halbach: The Logic Manual Chap 5
+
+;; European cities
+(def cities
+  {:univ #{:florence :stockholm :barcelona :london}
+   :a    [:func 0 :florence]
+   :b    [:func 0 :london]
+   :Q    [:pred 1 (make-pred #{[:florence] [:stockholm] [:barcelona]})]
+   :R    [:pred 2 (make-pred #{[:florence :london] 
+                               [:barcelona :london]
+                               [:stockholm :london]})]
+   })
+
+(def phi1 '(R a b))
+
+(eval-phi phi1 cities)
+; => true -- (R :florence :london)
+
+(eval-phi '(R b a) cities)
+; => false
+
+(def phi2 '(forall [x] (impl (Q x) (R x b))))
+
+(eval-phi phi2 cities)
+; => true
+
+(def phi3 '(forall [x] (exists [y] (or (R x y) (R y x)))))
+
+(eval-phi phi3 cities)
+; => true 
+
+;; world-5-9
+(def world-5-9
+  {:univ #{1 2}
+   :b    [:func 0 1]
+   :Q    [:pred 1 (make-pred #{[1]})]})
+
+(def phi4 '(impl (Q b) (forall [x] (Q x))))
+
+(eval-phi phi4 world-5-9)
+; => false
+
+;; cosmos
+(def cosmos
+  {:univ #{:sun :moon}
+   :R    [:pred 2 (make-pred #{[:sun :sun] [:moon :moon]})]})
+
+(def phi5 '(impl (forall [x] (exists [y] (R x y))) (exists [y] (forall [x] (R x y)))))
+
+(eval-phi phi5 cosmos)
+; => false
+
+;; world-5-11
+(def world-5-11
+  {:univ #{1}
+   :a    [:func 0 1]
+   :P    [:pred 1 (make-pred #{[1]})]
+   :Q    [:pred 1 (make-pred #{[1]})]
+   :R    [:pred 1 (make-pred #{})]})
+
+(def phi6 '(impl (and (forall [x] (impl (P x) (or (Q x) (R x)))) (P a)) (R a)))
+
+(eval-phi phi6 world-5-11)
+; => false
+
