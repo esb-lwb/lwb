d36cf4aca4f29c8b8df43dc5d8993bddc4af33fe
br <Burkhardt.Renz@mni.thm.de>
2016-04-08 09:38:11 +0200

    BDDFactory has to provided in (with-bddf ...) Improvement of interface of (build-bdd...) handling of border cases mapping of all operators from prop

--- src/lwb/prop/bdd.clj
+++ src/lwb/prop/bdd.clj
@@ -13,61 +13,131 @@
 
 ; Example
 (def phi1 '(and (or a b) (not (or c d))))
+(def phi2 '(and p (not p)))
+(def phi3 '(or p (not p)))
 
 ; function from prop
 (atoms-of-phi phi1)
 
-(defn atoms-as-bdds
+; All functions with binary decision diagrams have to be executed in the
+; context of an initialized BDDFactory, see the documentation of JavaBDD and buddy.
+; Due to the construction of buddy and JavaBDD the used factory is a singleton.
+(defmacro with-bddf
+  "`binding` is a vector that assigns a BDDFactory to a symbol.
+   The body is evaluated in a try block, finally the BDDFactory
+   is reset."
+  [binding & body]
+  `(let ~binding
+     (try ~@body
+     (catch Exception e# (str "caught: " (.getMessage e#)))
+     (finally (.done ~(binding 0))))))
+
+; Initializing the JFactory
+(defn init-bddf
+  "(init-bddf :small)   inits the JFactory for small formulae,
+   (init-bddf :medsize) inits the JFactory for medium size formulae,
+   (init-bddf :large)   inits the JFactory for large formulae,
+   (init-bddf nodesize cachesize) inits the JFactory
+   'Typical values for nodesize are 10.000 nodes for small test examples
+   and up to 1.000.000 nodes for large examples. A cache size of 10.000 seems
+   to work good even for large examples, but lesser values should do it for smaller
+   examples.' (from the documentation of bdd_init of buddy."
+  ([type]
+   (case type
+     :small (init-bddf 10000 1000))
+     :medsize (init-bddf 100000 10000)
+   (init-bddf 1000000 10000))
+  ([nodesize cachesize]
+   (JFactory/init nodesize cachesize)))
+
+(defn- atoms-as-bdds
   "the JavaBDD Factory `bddf` returns a map of BDD Objects for each atom of formula `phi`."
   [bddf phi]
   (let [atoms (atoms-of-phi phi), c (count atoms)]
-    (.setVarNum bddf c)
-    (zipmap atoms (map #(.ithVar bddf %) (range c)))))
+    (if (zero? c)
+      {}
+      (do
+        (.setVarNum bddf c)
+        (zipmap atoms (map #(.ithVar bddf %) (range c)))))))
 
-; TODO: extend such that we have all operators and even trivial cases like true, false
-(def functions
-  {'or #(.or %1 %2)
-   'and #(.and %1 %2)
-   'xor #(.xor %1 %2)
-   'not #(.not %1)
-   'ite #(.ite %1 %2 %3)
-   'impl #(.imp %1 %2)})
+(atoms-as-bdds (init-bddf :small) '(and p q))
+(atoms-as-bdds (init-bddf :small) 'true)
 
-(defn build-bdd
-  [atom-map phi]
-  (if (symbol? phi)
-    (get atom-map phi)
+(def ^:private functions
+  ^{:doc "mapping of operators to BDD functions"}
+  {'not   #(.not   ^BDD %1)
+   'and   #(.and   ^BDD %1 ^BDD %2)
+   'or    #(.or    ^BDD %1 ^BDD %2)
+   'impl  #(.imp   ^BDD %1 ^BDD %2)
+   'equiv #(.biimp ^BDD %1 ^BDD %2)
+   'xor   #(.xor   ^BDD %1 ^BDD %2)
+   'ite   #(.ite   ^BDD %1 ^BDD %2 ^BDD %3)})
+
+(defn- build-bdd-inner
+  [bddf atom-map phi]
+  (if (simple-expr? phi)
+    (cond (= phi 'true) (.one bddf)
+          (= phi 'false) (.zero bddf)
+          :else (get atom-map phi))
     (let [op (first phi)]
       (case (arity op)
-        1 ((functions op) (build-bdd atom-map (second phi)))
-        2 ((functions op) (build-bdd atom-map (second phi))
-            (build-bdd atom-map (nth phi 2)))
-        3 ((functions op) (build-bdd atom-map (second phi))
-            (build-bdd atom-map (nth phi 2))
-            (build-bdd atom-map (nth phi 3)))
-        -1 (reduce (functions op) (map #(build-bdd atom-map %1) (rest phi)))
+        1 ((functions op) (build-bdd-inner bddf atom-map (second phi)))
+        2 ((functions op) (build-bdd-inner bddf atom-map (second phi))
+            (build-bdd-inner bddf atom-map (nth phi 2)))
+        3 ((functions op) (build-bdd-inner bddf atom-map (second phi))
+            (build-bdd-inner bddf atom-map (nth phi 2))
+            (build-bdd-inner bddf atom-map (nth phi 3)))
+        -1 (reduce (functions op) (map #(build-bdd-inner bddf atom-map %1) (rest phi)))
         ))))
+
+(defn build-bdd
+  [bddf phi]
+  (let [atom-map (atoms-as-bdds bddf phi)]
+    (build-bdd-inner bddf atom-map phi)))
+
+(type 'false)
+(build-bdd (init-bddf :small) '(and p q))
+(build-bdd (init-bddf :small) 'p)
+(build-bdd (init-bddf :small) 'true)
+(build-bdd (init-bddf :small) 'false)
 
 ; TODO: represent the table as a clojure object
 (defn bdd-table
-  [phi nodenum]
-  (let [bddf (JFactory/init nodenum 10000)
-        atom-map (atoms-as-bdds bddf phi)
-        bdd  (build-bdd atom-map phi)]
-    (.printTable bddf bdd)
-    (.done bddf)))
+  [bddf phi]
+  (let [bdd  (build-bdd bddf phi)]
+    (.printTable bddf bdd)))
 
-(bdd-table phi1 100000)
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-table bddf phi1))
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-table bddf phi2))
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-table bddf phi3))
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-table bddf '(equiv p q)))
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-table bddf '(impl p q)))
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-table bddf '(or (not p) q)))
+
+(wff? 'true)
+
+
+
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-table bddf 'true))
 
 ; TODO: use the clojure representation of a bdd to generate dot code
 (defn bdd-dot
-  [phi nodenum]
-  (let [bddf (JFactory/init nodenum 10000)
-        atom-map (atoms-as-bdds bddf phi)
-        bdd  (build-bdd atom-map phi)]
-    (.printDot bdd)
-    (.done bddf)))
+  [bddf phi]
+  (let [bdd  (build-bdd bddf phi)]
+    (.printDot bdd)))
 
-(bdd-dot phi1 100000)
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-dot bddf phi1))
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-dot bddf phi2))
+(with-bddf [bddf (init-bddf :small)]
+           (bdd-dot bddf phi3))
 
 ; TODO: sat with bdd
