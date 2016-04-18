7e1f42ea5357fbb5a09ef85aa46b1e38c4cf1615
br <Burkhardt.Renz@mni.thm.de>
2016-04-18 15:33:18 +0200

    first example sat

---
+++ src/lwb/prop/examples/sat.clj
@@ -0,0 +1,29 @@
+; lwb Logic WorkBench -- Propositional Logic 
+; Examples: Binary decision diagrams
+
+; Copyright (c) 2016 Burkhardt Renz, THM. ; All rights reserved.
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php).
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+
+(ns lwb.prop.examples.sat
+  (:require [lwb.prop :refer :all])
+  (:require [lwb.prop.sat :refer :all]))
+
+; R.L.Rivest's associative block design, see Knuth TAOCP Vol.4 Fac. 6 p. 4
+
+(def R8 '(and (or x_1 x_2 (not x_3)) (or x_2 x_3 (not x_4)) (or x_3 x_4 x_1) (or x_4 (not x_1) x_2)
+              (or (not x_1) (not x_2) x_3) (or (not x_2) (not x_3) x_4) (or (not x_3) (not x_4) (not x_1))
+              (or (not x_4) x_1 (not x_2))))
+
+(sat? R8)
+
+(def R7 '(and (or x_1 x_2 (not x_3)) (or x_2 x_3 (not x_4)) (or x_3 x_4 x_1) (or x_4 (not x_1) x_2)
+              (or (not x_1) (not x_2) x_3) (or (not x_2) (not x_3) x_4) (or (not x_3) (not x_4) (not x_1))))
+
+(sat? R7)
+
+(sat R7 :all)
+; => ([x_1 false x_2 true x_3 false x_4 true]
+;     [x_1 false x_2 true x_3 true x_4 true])
