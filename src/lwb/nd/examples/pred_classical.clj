696dbe4dc82bde7d3176b6c3b809102ab8eb2e7a
br <Burkhardt.Renz@mni.thm.de>
2016-11-04 13:59:09 +0100

    resultes of classical proofs check of exists-i

--- src/lwb/nd/examples/pred_classical.clj
+++ src/lwb/nd/examples/pred_classical.clj
@@ -29,6 +29,25 @@
 
 ;(export-theorem "resources/nd/theorems-pred.edn" :not-forall->exists-not)
 
+;    --------------------------------------------------
+;  1: (not (forall [x] (P x)))                :premise
+;      ------------------------------------------------
+;  2:  | (not (exists [x] (not (P x))))       :assumption
+;      | ----------------------------------------------
+;  3:  | | (actual :i)                        :assumption
+;      | | --------------------------------------------
+;  4:  | | | (not (P :i))                     :assumption
+;  5:  | | | (exists [x] (not (P x)))         :exists-i [3 4]
+;  6:  | | | contradiction                    :not-e [2 5]
+;      | | --------------------------------------------
+;  7:  | | (P :i)                             :raa [[4 6]]
+;      | ----------------------------------------------
+;  8:  | (forall [x] (P x))                   :forall-i [[3 7]]
+;  9:  | contradiction                        :not-e [1 8]
+;      ------------------------------------------------
+; 10: (exists [x] (not (P x)))                :raa [[2 9]]
+;    --------------------------------------------------
+
 ; exists-not->not-forall
 
 (proof '(exists [x] (not (P x))) '(not (forall [x] (P x))))
@@ -39,6 +58,21 @@
 (step-f :not-e 4 5)
 
 ;(export-theorem "resources/nd/theorems-pred.edn" :exists-not->not-forall)
+
+;    --------------------------------------------------
+; 1: (exists [x] (not (P x)))                :premise
+;     ------------------------------------------------
+; 2:  | (forall [x] (P x))                   :assumption
+;     | ----------------------------------------------
+; 3:  | | (actual :i)                        :assumption
+; 4:  | | (not (P :i))                       :assumption
+; 5:  | | (P :i)                             :forall-e [2 3]
+; 6:  | | contradiction                      :not-e [4 5]
+;     | ----------------------------------------------
+; 7:  | contradiction                        :exists-e [1 [3 6]]
+;     ------------------------------------------------
+; 8: (not (forall [x] (P x)))                :not-i [[2 7]]
+;    --------------------------------------------------
 
 ; not-exists->forall-not
 
@@ -51,6 +85,20 @@
 
 ;(export-theorem "resources/nd/theorems-pred.edn" :not-exists->forall-not)
 
+;    --------------------------------------------------
+; 1: (not (exists [x] (P x)))                :premise
+;     ------------------------------------------------
+; 2:  | (actual :i)                          :assumption
+;     | ----------------------------------------------
+; 3:  | | (P :i)                             :assumption
+; 4:  | | (exists [x] (P x))                 :exists-i [2 3]
+; 5:  | | contradiction                      :not-e [1 4]
+;     | ----------------------------------------------
+; 6:  | (not (P :i))                         :not-i [[3 5]]
+;     ------------------------------------------------
+; 7: (forall [x] (not (P x)))                :forall-i [[2 6]]
+;    --------------------------------------------------
+
 ; forall-not->not-exists
 
 (proof '(forall [x] (not (P x))) '(not (exists [x] (P x))))
@@ -61,6 +109,21 @@
 (step-f :not-e 5 4)
 
 ;(export-theorem "resources/nd/theorems-pred.edn" :forall-not->not-exists)
+
+;    --------------------------------------------------
+; 1: (forall [x] (not (P x)))                :premise
+;     ------------------------------------------------
+; 2:  | (exists [x] (P x))                   :assumption
+;     | ----------------------------------------------
+; 3:  | | (actual :i)                        :assumption
+; 4:  | | (P :i)                             :assumption
+; 5:  | | (not (P :i))                       :forall-e [1 3]
+; 6:  | | contradiction                      :not-e [5 4]
+;     | ----------------------------------------------
+; 7:  | contradiction                        :exists-e [2 [3 6]]
+;     ------------------------------------------------
+; 8: (not (exists [x] (P x)))                :not-i [[2 7]]
+;    --------------------------------------------------
 
 ; -----------------------------------------------------------------------------------------
 ; Laws of equality
@@ -112,6 +175,23 @@
 
 ;(export-theorem "resources/nd/theorems-pred.edn" :modus-barbara)
 
+;     --------------------------------------------------
+;  1: (forall [x] (impl (P x) (Q x)))         :premise
+;  2: (forall [x] (impl (Q x) (R x)))         :premise
+;      ------------------------------------------------
+;  3:  | (actual :i)                          :assumption
+;  4:  | (impl (P :i) (Q :i))                 :forall-e [1 3]
+;  5:  | (impl (Q :i) (R :i))                 :forall-e [2 3]
+;      | ----------------------------------------------
+;  6:  | | (P :i)                             :assumption
+;  7:  | | (Q :i)                             :impl-e [4 6]
+;  8:  | | (R :i)                             :impl-e [5 7]
+;      | ----------------------------------------------
+;  9:  | (impl (P :i) (R :i))                 :impl-i [[6 8]]
+;      ------------------------------------------------
+; 10: (forall [x] (impl (P x) (R x)))         :forall-i [[3 9]]
+;     --------------------------------------------------
+
 ; Modus Celarent 
 ; "EAE
 
@@ -126,33 +206,80 @@
 
 ;(export-theorem "resources/nd/theorems-pred.edn" :modus-celarent)
 
+;     --------------------------------------------------
+;  1: (forall [x] (impl (P x) (not (Q x))))   :premise
+;  2: (forall [x] (impl (R x) (P x)))         :premise
+;      ------------------------------------------------
+;  3:  | (actual :i)                          :assumption
+;  4:  | (impl (P :i) (not (Q :i)))           :forall-e [1 3]
+;  5:  | (impl (R :i) (P :i))                 :forall-e [2 3]
+;      | ----------------------------------------------
+;  6:  | | (R :i)                             :assumption
+;  7:  | | (P :i)                             :impl-e [5 6]
+;  8:  | | (not (Q :i))                       :impl-e [4 7]
+;      | ----------------------------------------------
+;  9:  | (impl (R :i) (not (Q :i)))           :impl-i [[6 8]]
+;      ------------------------------------------------
+; 10: (forall [x] (impl (R x) (not (Q x))))   :forall-i [[3 9]]
+;     --------------------------------------------------
+
 ; Modus Darii
 ; "AII"
 
 (proof '[(forall [x] (impl (P x) (Q x))) (exists [x] (and (R x) (P x)))] '(exists [x] (and (R x) (Q x))))
-(step-f :exists-e 2)
+(step-f :exists-e 2 4)
 (unify 'V1 :i)
 (step-f :forall-e 1 3)
-(unify 'V2 '(exists [x] (and (R x) (Q x))))
 (step-f :and-e2 4)
 (step-f :impl-e 5 6)
 (step-f :and-e1 4)
 (step-f :and-i 8 7)
 (step-b :exists-i 11 3)
+
+;     --------------------------------------------------
+;  1: (forall [x] (impl (P x) (Q x)))         :premise
+;  2: (exists [x] (and (R x) (P x)))          :premise
+;      ------------------------------------------------
+;  3:  | (actual :i)                          :assumption
+;  4:  | (and (R :i) (P :i))                  :assumption
+;  5:  | (impl (P :i) (Q :i))                 :forall-e [1 3]
+;  6:  | (P :i)                               :and-e2 [4]
+;  7:  | (Q :i)                               :impl-e [5 6]
+;  8:  | (R :i)                               :and-e1 [4]
+;  9:  | (and (R :i) (Q :i))                  :and-i [8 7]
+; 10:  | (exists [x] (and (R x) (Q x)))       :exists-i [3 9]
+;      ------------------------------------------------
+; 11: (exists [x] (and (R x) (Q x)))          :exists-e [2 [3 10]]
+;     --------------------------------------------------
 
 
 ; Modus Ferio
 ; "EIO"
 
 (proof '[(forall [x] (impl (P x) (not (Q x)))) (exists [x] (and (R x) (P x)))] '(exists [x] (and (R x) (not (Q x)))))
-(step-f :exists-e 2)
+(step-f :exists-e 2 4)
 (unify 'V1 :i)
-(unify 'V2 '(exists [x] (and (R x) (not (Q x)))))
 (step-f :forall-e 1 3)
 (step-f :and-e2 4)
 (step-f :impl-e 5 6)
 (step-f :and-e1 4)
 (step-f :and-i 8 7)
 (step-b :exists-i 11 3)
+
+;     --------------------------------------------------
+;  1: (forall [x] (impl (P x) (not (Q x))))   :premise
+;  2: (exists [x] (and (R x) (P x)))          :premise
+;      ------------------------------------------------
+;  3:  | (actual :i)                          :assumption
+;  4:  | (and (R :i) (P :i))                  :assumption
+;  5:  | (impl (P :i) (not (Q :i)))           :forall-e [1 3]
+;  6:  | (P :i)                               :and-e2 [4]
+;  7:  | (not (Q :i))                         :impl-e [5 6]
+;  8:  | (R :i)                               :and-e1 [4]
+;  9:  | (and (R :i) (not (Q :i)))            :and-i [8 7]
+; 10:  | (exists [x] (and (R x) (not (Q x)))) :exists-i [3 9]
+;      ------------------------------------------------
+; 11: (exists [x] (and (R x) (not (Q x))))    :exists-e [2 [3 10]]
+;     --------------------------------------------------
 
 
