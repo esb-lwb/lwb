a29f368a8613d479760017a5f759e19227b7d988
br <Burkhardt.Renz@mni.thm.de>
2016-11-04 13:28:52 +0100

    substitution?

--- src/lwb/nd/prereqs.clj
+++ src/lwb/nd/prereqs.clj
@@ -1,15 +1,19 @@
-; lwb Logic WorkBench -- Natural deduction
+; lwb Logic WorkBench -- Natural deduction 
 
-; Copyright (c) 2015 Tobias Völzel, THM. All rights reserved.
+; Copyright (c) 2015 - 2016 Tobias Völzel, Burkhardt Renz, THM. All rights reserved.
 ; The use and distribution terms for this software are covered by the
 ; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php).
 ; By using this software in any fashion, you are agreeing to be bound by
 ; the terms of this license.
 
 (ns lwb.nd.prereqs
-  (:require [clojure.zip :as zip]))
+  (:require [lwb.pred.substitution :refer [substitution]]))
 
 (defn substitution?
-  "Is `phi'` the substitution of `var` in `phi`?"
+  "Is `phi'` the substitution of `var` in `phi`?    
+   Throws exception if that's not the case."
   [phi' phi var term]
-  true)
+  (if (= phi' (substitution phi var term))
+    true
+    (throw (Exception. (format "%s should be the substitution of %s in %s by %s" phi' var phi term)))))
+
