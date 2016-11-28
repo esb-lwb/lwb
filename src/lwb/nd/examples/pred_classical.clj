eaac6ce27cca10c0474c603d08be74ddb2fe117c
br <Burkhardt.Renz@mni.thm.de>
2016-11-28 14:43:28 +0100

    aux rule: we can introduce an actual element of the universe, since we always have a non-empty universe Caveat: the user has to check the usage of this!!
    A rather complete set of basic theorems of predicate logic

--- src/lwb/nd/examples/pred_classical.clj
+++ src/lwb/nd/examples/pred_classical.clj
@@ -27,7 +27,7 @@
 (step-b :not-e 6 2)
 (step-b :exists-i 6 3)
 
-; (export "resources/nd/theorems-pred.edn" :not-forall->exists-not)
+;(export "resources/nd/theorems-pred.edn" :not-forall->exists-not)
 
 ;    --------------------------------------------------
 ;  1: (not (forall [x] (P x)))                :premise
@@ -70,7 +70,7 @@
 (step-f :forall-e 2 3)
 (step-f :not-e 4 5)
 
-; (export "resources/nd/theorems-pred.edn" :exists-not->not-forall)
+;(export "resources/nd/theorems-pred.edn" :exists-not->not-forall)
 
 ;    --------------------------------------------------
 ; 1: (exists [x] (not (P x)))                :premise
@@ -101,7 +101,7 @@
 (step-b :not-e 5 1)
 (step-b :exists-i 5 2)
 
-; (export "resources/nd/theorems-pred.edn" :not-exists->forall-not)
+;(export "resources/nd/theorems-pred.edn" :not-exists->forall-not)
 
 ;    --------------------------------------------------
 ; 1: (not (exists [x] (P x)))                :premise
@@ -126,7 +126,7 @@
 (step-f :forall-e 1 3)
 (step-f :not-e 5 4)
 
-; (export "resources/nd/theorems-pred.edn" :forall-not->not-exists)
+;(export "resources/nd/theorems-pred.edn" :forall-not->not-exists)
 
 ;    --------------------------------------------------
 ; 1: (forall [x] (not (P x)))                :premise
@@ -144,6 +144,253 @@
 ;    --------------------------------------------------
 
 ; -----------------------------------------------------------------------------------------
+; Commutativity
+
+(proof '(forall [x] (forall [y] (A x y))) '(forall [y] (forall [x] (A x y))))
+(step-b :forall-i 3)
+(swap '?1 :y)
+(step-b :forall-i 4)
+(swap '?2 :x)
+(step-f :forall-e 1 3)
+(step-f :forall-e 4 2)
+
+;(export "resources/nd/theorems-pred.edn" :forall-comm1)
+
+(proof '(forall [y] (forall [x] (A x y))) '(forall [x] (forall [y] (A x y))))
+(step-b :forall-i 3)
+(swap '?1 :x)
+(step-b :forall-i 4)
+(swap '?2 :y)
+(step-f :forall-e 1 3)
+(step-f :forall-e 4 2)
+
+;(export "resources/nd/theorems-pred.edn" :forall-comm2)
+
+(proof '(exists [x] (exists [y] (A x y))) '(exists [y] (exists [x] (A x y))))
+(step-f :exists-e 1 3)
+(swap '?1 :x)
+(step-f :exists-e 3 5)
+(swap '?2 :y)
+(step-b :exists-i 7 4)
+(step-b :exists-i 7 2)
+
+;(export "resources/nd/theorems-pred.edn" :exists-comm1)
+
+(proof '(exists [y] (exists [x] (A x y))) '(exists [x] (exists [y] (A x y))))
+(step-b :exists-i 7 2)
+(step-f :exists-e 1 3)
+(swap '?1 :y)
+(step-f :exists-e 3 5)
+(swap '?2 :x)
+(step-b :exists-i 7 4)
+(step-b :exists-i 7 2)
+
+;(export "resources/nd/theorems-pred.edn" :exists-comm2)
+
+(proof '(exists [x] (forall [y] (A x y))) '(forall [y] (exists [x] (A x y))))
+(step-b :forall-i 3)
+(swap '?1 :y)
+(step-f :exists-e 1 4)
+(swap '?2 :x)
+(step-f :forall-e 4 2)
+(step-b :exists-i 7 3)
+
+;(export "resources/nd/theorems-pred.edn" :exists-forall-comm)
+
+; -----------------------------------------------------------------------------------------
+; Distributivity
+
+(proof '(exists [x] (or (A x) (B x))) '(or (exists [x] (A x)) (exists [x] (B x))))
+(step-f :exists-e 1 3)
+(swap '?1 :x)
+(step-f :or-e 3 5)
+(step-b :or-i1 6)
+(step-b :exists-i 6 2)
+(step-b :or-i2 9)
+(step-b :exists-i 9 2)
+
+;(export "resources/nd/theorems-pred.edn" :exists-or-dist1)
+
+(proof '(or (exists [x] (A x)) (exists [x] (B x))) '(exists [x] (or (A x) (B x))))
+(step-f :or-e 1 3)
+(step-f :exists-e 2 4)
+(swap '?1 :x)
+(step-b :exists-i 6 3)
+(step-b :or-i1 6)
+(step-f :exists-e 8 10)
+(swap '?2 :x)
+(step-b :exists-i 12 9)
+(step-b :or-i2 12)
+
+;(export "resources/nd/theorems-pred.edn" :exists-or-dist2)
+
+(proof '(forall [x] (and (A x) (B x))) '(and (forall [x] (A x)) (forall [x] (B x))))
+(step-b :and-i 3)
+(step-b :forall-i 3)
+(swap '?1 :x)
+(step-f :forall-e 1 2)
+(step-f :and-e1 3)
+(step-b :forall-i 7)
+(swap '?2 :x)
+(step-f :forall-e 1 6)
+(step-f :and-e2 3)
+
+;(export "resources/nd/theorems-pred.edn" :forall-and-dist1)
+
+(proof '(and (forall [x] (A x)) (forall [x] (B x))) '(forall [x] (and (A x) (B x))))
+(step-f :and-e1 1)
+(step-f :and-e2 1)
+(step-b :forall-i 5)
+(swap '?1 :x)
+(step-f :forall-e 2 4)
+(step-f :forall-e 3 4)
+(step-b :and-i 8)
+
+;(export "resources/nd/theorems-pred.edn" :forall-and-dist2)
+
+(proof '(exists [x] (and (A x) (B x))) '(and (exists [x] (A x)) (exists [x] (B x))))
+(step-f :exists-e 1 3)
+(swap '?1 :x)
+(step-f :and-e1 3)
+(step-f :and-e2 3)
+(step-b :and-i 7)
+(step-b :exists-i 7 2)
+(step-b :exists-i 8 2)
+
+;(export "resources/nd/theorems-pred.edn" :exists-and-dist)
+
+(proof '(or (forall [x] (A x)) (forall [x] (B x))) '(forall [x] (or (A x) (B x))))
+(step-f :or-e 1 3)
+(step-b :forall-i 4)
+(swap '?1 :x)
+(step-f :forall-e 2 3)
+(step-b :or-i1 6)
+(step-b :forall-i 9)
+(swap '?2 :x)
+(step-f :forall-e 7 8)
+(step-b :or-i2 11)
+
+;(export "resources/nd/theorems-pred.edn" :forall-or-dist)
+
+; -----------------------------------------------------------------------------------------
+; Quantification without free variable in scope
+
+(proof '(or (exists [x] (A x)) B) '(exists [x] (or (A x) B)))
+(step-f :or-e 1 3)
+(step-f :exists-e 2 4)
+(swap '?1 :x)
+(step-b :exists-i 6 3)
+(step-b :or-i1 6)
+(step-f :actual)
+(swap '?2 :x)
+(step-b :exists-i 11 9)
+(step-b :or-i2 11)
+; that's okay, since we do not use whether (A :x) is true or not!!
+
+(proof '(exists [x] (or (A x) B)) '(or (exists [x] (A x)) B))
+(step-f :exists-e 1 3)
+(swap '?1 :x)
+(step-f :or-e 3 5)
+(subclaim '(exists [x] (A x)))
+(step-b :exists-i 6 2)
+(step-b :or-i1 7)
+(step-b :or-i2 9)
+
+(proof '(and (exists [x] (A x)) B) '(exists [x] (and (A x) B)))
+(step-f :and-e1 1)
+(step-f :and-e2 1)
+(step-f :exists-e 2 5)
+(swap '?1 :x)
+(step-f :and-i 5 3)
+(step-b :exists-i 8 4)
+
+(proof '(exists [x] (and (A x) B)) '(and (exists [x] (A x)) B))
+(step-f :exists-e 1 3)
+(swap '?1 :x)
+(step-f :and-e1 3)
+(step-f :and-e2 3)
+(subclaim '(exists [x] (A x)))
+(step-b :exists-i 7 2)
+(step-b :and-i 8)
+
+(proof '(or (forall [x] (A x)) B) '(forall [x] (or (A x) B)))
+(step-f :or-e 1 3)
+(step-b :forall-i 4)
+(swap '?1 :x)
+(step-f :forall-e 2 3)
+(step-b :or-i1 6)
+(step-b :forall-i 9)
+(swap '?2 :x)
+(step-b :or-i2 10)
+
+(proof '(forall [x] (or (A x) B)) '(or (forall [x] (A x)) B))
+(step-f :tnd)
+(swap '?1 'B)
+(step-f :or-e 2 4)
+(step-b :or-i2 5)
+(step-b :or-i1 7)
+(step-b :forall-i 7)
+(swap '?2 :x)
+(step-f :forall-e 1 6)
+(step-f :or-e 7 9)
+(step-f :not-e 5 10)
+(step-b :efq 13)
+
+(proof '(and (forall [x] (A x)) B) '(forall [x] (and (A x) B)))
+(step-f :and-e1 1)
+(step-f :and-e2 1)
+(step-b :forall-i 5)
+(swap '?1 :x)
+(step-f :forall-e 2 4)
+(step-b :and-i 7)
+
+(proof '(forall [x] (and (A x) B)) '(and (forall [x] (A x)) B))
+(step-f :actual)
+(swap '?1 :x)
+(step-f :forall-e 1 2)
+(step-f :and-e2 3)
+(step-b :and-i 6)
+(step-b :forall-i 6)
+(swap '?2 :y)
+(step-f :forall-e 1 5)
+(step-f :and-e1 6)
+
+(proof '(forall [x] (impl (A x) B)) '(impl (exists [x] (A x)) B))
+(step-b :impl-i 3)
+(step-f :exists-e 2 4)
+(swap '?1 :x)
+(step-f :forall-e 1 3)
+(step-f :impl-e 5 4)
+
+(proof '(impl (exists [x] (A x)) B) '(forall [x] (impl (A x) B)))
+(step-b :forall-i 3)
+(swap '?1 :x)
+(step-f :tnd)
+(swap '?2 '(exists [x] (A x)))
+(step-f :or-e 3 5)
+(step-f :impl-e 1 4)
+(step-b :impl-i 7)
+(step-f :not-exists->forall-not 9)
+(step-f :forall-e 10 2)
+(step-b :impl-i 13)
+(step-f :not-e 11 12)
+(step-b :efq 15)
+
+(proof '(forall [x] (impl A (B x))) '(impl A (forall [x] (B x))))
+(step-b :impl-i 3)
+(step-b :forall-i 4)
+(swap '?1 :x)
+(step-f :forall-e 1 3)
+(step-f :impl-e 4 2)
+
+(proof '(impl A (forall [x] (B x))) '(forall [x] (impl A (B x))))
+(step-b :forall-i 3)
+(swap '?1 :x)
+(step-b :impl-i 4)
+(step-f :impl-e 1 3)
+(step-f :forall-e 4 2)
+
+; -----------------------------------------------------------------------------------------
 ; Laws of equality
 
 ; equal-refl: equality is reflexiv
@@ -153,7 +400,7 @@
 (swap '?1 :t1)
 (step-f :equal-e 1 2 '(= x :t1) 'x)
 
-;(export "resources/nd/theorems-pred.edn" :equals-refl)
+; (export "resources/nd/theorems-pred.edn" :equal-refl)
 
 ;
 ; --------------------------------------------------
@@ -167,7 +414,7 @@
 (proof '[(= :t1 :t2) (= :t2 :t3)] '(= :t1 :t3))
 (step-f :equal-e 2 1 '(= :t1 x) 'x)
 
-;(export "resources/nd/theorems-pred.edn" :equal-refl)
+;(export "resources/nd/theorems-pred.edn" :equal-trans)
 
 ;
 ; --------------------------------------------------
@@ -195,7 +442,7 @@
 (step-f :impl-e 4 6)
 (step-f :impl-e 5 7)
 
-; (export "resources/nd/theorems-pred.edn" :modus-barbara)
+;(export "resources/nd/theorems-pred.edn" :modus-barbara)
 
 ;     --------------------------------------------------
 ;  1: (forall [x] (impl (P x) (Q x)))         :premise
@@ -226,7 +473,7 @@
 (step-f :impl-e 5 6)
 (step-f :impl-e 4 7)
 
-; (export "resources/nd/theorems-pred.edn" :modus-celarent)
+;(export "resources/nd/theorems-pred.edn" :modus-celarent)
 
 ;     --------------------------------------------------
 ;  1: (forall [x] (impl (P x) (not (Q x))))   :premise
@@ -258,7 +505,7 @@
 (step-f :and-i 8 7)
 (step-b :exists-i 11 3)
 
-; (export "resources/nd/theorems-pred.edn" :modus-darii)
+;(export "resources/nd/theorems-pred.edn" :modus-darii)
 
 ;     --------------------------------------------------
 ;  1: (forall [x] (impl (P x) (Q x)))         :premise
@@ -289,7 +536,7 @@
 (step-f :and-i 8 7)
 (step-b :exists-i 11 3)
 
-; (export "resources/nd/theorems-pred.edn" :modus-ferio)
+;(export "resources/nd/theorems-pred.edn" :modus-ferio)
 
 ;     --------------------------------------------------
 ;  1: (forall [x] (impl (P x) (not (Q x))))   :premise
