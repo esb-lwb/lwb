af2f56b0241f921bb1d35d2261b0e2b711b77f7a
br <Burkhardt.Renz@mni.thm.de>
2016-11-28 14:35:00 +0100

    error filterv str/join writing theorems

--- src/lwb/nd/proof.clj
+++ src/lwb/nd/proof.clj
@@ -273,7 +273,7 @@
   [proof]
   (let [pm (plid-plno-map proof)
         fn (fn [pline] (let [curr-plno (get pm (:plid pline))
-                             refs-plnos (mapv #(get pm %) (flatten (:refs pline)))
+                             refs-plnos (mapv #(get pm %) (filter number? (flatten (:refs pline))))
                              max-ref-plno (if (empty? refs-plnos) 0 (apply max refs-plnos))]
                          (< max-ref-plno curr-plno)))]
     (every? true? (map fn (flatten proof)))))
