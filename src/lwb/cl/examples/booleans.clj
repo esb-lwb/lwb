fa6707f2683e1d6d2fa894d085291feab5576f8f
br <Burkhardt.Renz@mni.thm.de>
2019-04-06 15:13:30 +0200

    booleans in Church encoding

---
+++ src/lwb/cl/examples/booleans.clj
@@ -0,0 +1,58 @@
+; lwb Logic WorkBench -- Church encoding of booleans
+
+; Copyright (c) 2019  Burkhardt Renz, THM. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php).
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+
+(ns lwb.cl.examples.booleans
+  (:require [lwb.cl :refer :all]))
+
+(def-combinators-ski)
+
+;; Definition of truth values
+
+(def True '[K])
+(def False '[K I])
+
+(defn to-boolean
+  [term]
+  (let [t (min-parens term)]
+    (cond
+      (= t True) 'True
+      (= t False) 'False
+      :else nil)))
+
+;; Definition of operators
+
+; helper function
+(defn- conj'
+  [& terms]
+  (min-parens (into [] (reduce concat (map max-parens terms)))))
+
+(defn And ; = [S S K]
+  [term1 term2]
+    (to-boolean (weak-reduce (conj' '[S S K] term1 term2))))
+  
+(And True True)
+(And True False)
+(And False True)
+(And False False)
+
+(defn Or ;  = [S I I]
+  [term1 term2]
+  (to-boolean (weak-reduce (conj' '[S I I] term1 term2))))
+  
+(Or True True)
+(Or True False)
+(Or False True)
+(Or False False)
+
+(defn Not ;  = [S (S I (K (K I))) (K K)] (in normal order)
+  [term]
+  (to-boolean (weak-reduce (conj' '[S (S I (K (K I))) (K K)] term))))
+
+(Not True)
+(Not False)
+  
