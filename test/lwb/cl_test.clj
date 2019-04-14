5420fca260745e5cae0a1e4115f68b24fba92665
br <Burkhardt.Renz@mni.thm.de>
2019-04-14 09:57:27 +0200

    plan for tests

---
+++ test/lwb/cl_test.clj
@@ -0,0 +1,65 @@
+(ns lwb.cl-test
+  (:require [lwb.cl :refer :all]
+            [clojure.test :refer :all]))
+
+;; Syntax ----------------------------------------------------------------
+
+;; combinator?
+
+;; variable?
+
+;; wff?
+
+;; variables
+
+;; combinators
+
+;; vfree?
+
+;; size
+
+;; Parentheses -----------------------------------------------------------
+
+;; max-parens
+
+;; min-paerns
+
+;; Temrs -----------------------------------------------------------------
+
+;; subst
+
+;; subterms
+
+;; comb-concat
+
+;; Definition ------------------------------------------------------------
+
+;; def-combinator
+
+;; comb-defined?
+
+;; show-combinator
+
+;; show-combinators
+
+;; reset-combinators
+
+;; def-combinators-ski
+
+;; def-combinatory-birds
+
+;; One-step reduction and expansion -----------------------------------
+
+;; one-step-red
+
+;; one-step-exp
+
+;; Weak reduction -----------------------------------------------------
+
+;; weak-reduce
+
+;; weak-reduce-multi
+
+;; Bracket abstraction ------------------------------------------------
+
+;; abstract
